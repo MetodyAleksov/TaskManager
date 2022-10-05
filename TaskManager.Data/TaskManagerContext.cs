62a10047f94769a9de44acb20d41f0b57b9653ee
Metody Aleksov <teddy57603@gmail.com>
2022-10-05 17:19:07 +0300

    added db context

---
+++ TaskManager.Data/TaskManagerContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManager.Data.Models;
+
+namespace TaskManager.Data
+{
+    public class TaskManagerContext : DbContext 
+    {
+        public TaskManagerContext(DbContextOptions<TaskManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
