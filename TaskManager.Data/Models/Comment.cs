a4ba7775e3b525e8c11200e6124b1f27a3a5d4b9
Metody Aleksov <teddy57603@gmail.com>
2022-10-05 17:19:15 +0300

    created models

---
+++ TaskManager.Data/Models/Comment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TaskManager.Data.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Content { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Type { get; set; }
+
+        public DateTime? ReminderDate { get; set; }
+
+        [ForeignKey(nameof(Task))]
+        public int TaskId { get; set; }
+        public Task Task { get; set; }
+    }
+}
---
+++ TaskManager.Data/Models/Task.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TaskManager.Data.Models
+{
+    public class Task
+    {
+        public Task()
+        {
+            Comments = new HashSet<Comment>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime TimeCreated { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Statuses { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string TaskTypes { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
