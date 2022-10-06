bc38045756dc7f7e881f8a69276c24c7dc493b91
Metody Aleksov <teddy57603@gmail.com>
2022-10-06 11:38:37 +0300

    created dtos for object in the database

---
+++ TaskManager.Data/DTOs/TaskDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Data.DTOs
+{
+    public class TaskDTO
+    {
+        public TaskDTO()
+        {
+            Statuses = new List<string>();
+            TaskTypes = new List<string>();
+            Comments = new List<CommentDTO>();
+        }
+
+        public DateTime TimeCreated { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Description { get; set; }
+        public IList<string> Statuses { get; set; }
+        public IList<string> TaskTypes { get; set; }
+        public string Authors { get; set; }
+        public IList<CommentDTO> Comments { get; set; }
+    }
+}
---
+++ TaskManager.Data/DTOs/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Data.DTOs
+{
+    public class UserDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
