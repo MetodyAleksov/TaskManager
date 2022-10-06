18a678cba508c402d021a360a72972f949f0dde1
Metody Aleksov <teddy57603@gmail.com>
2022-10-06 11:37:15 +0300

    added user controller

---
+++ TaskManager/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace TaskManager.Controllers
+{
+    public class UserController : Controller
+    {
+        private IConfiguration _config;
+
+        public UserController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string username, string password)
+        {
+            return View();
+        }
+    }
+}
