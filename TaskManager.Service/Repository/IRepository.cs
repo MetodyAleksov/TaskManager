94f2279045cbe98bc7ec804eee48476c10dc3a6f
Metody Aleksov <teddy57603@gmail.com>
2022-10-08 10:12:59 +0300

    added repository

---
+++ TaskManager.Service/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Service.Repository
+{
+    public interface IRepository
+    {
+        System.Threading.Tasks.Task AddAsync<T>(T entity) where T : class;
+
+        IQueryable<T> All<T>() where T : class;
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ TaskManager.Service/Repository/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Data;
+
+namespace TaskManager.Service.Repository
+{
+    public class Repository : IRepository
+    {
+
+        private readonly DbContext dbContext;
+
+        public Repository(TaskManagerContext context)
+        {
+            dbContext = context;
+        }
+
+        public async System.Threading.Tasks.Task AddAsync<T>(T entity) where T : class
+        {
+            await dbContext.AddAsync(entity);
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>().AsQueryable();
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await dbContext.SaveChangesAsync();
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return dbContext.Set<T>();
+        }
+
+
+    }
+}
