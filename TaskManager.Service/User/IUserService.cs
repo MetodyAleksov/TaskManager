dcc456c4af24fdef4a611a1d0cd83a458b66c2d7
Metody Aleksov <teddy57603@gmail.com>
2022-10-08 10:13:08 +0300

    added user service

---
+++ TaskManager.Service/User/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManager.Data.DTOs;
+
+namespace TaskManager.Service.User
+{
+    public interface IUserService
+    {
+        System.Threading.Tasks.Task CreateUser(string username, string password);
+        UserDTO GetUser(string username);
+        UserDTO[] GetAllUsers();
+    }
+}
---
+++ TaskManager.Service/User/UserService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskManager.Service.Repository;
+using TaskManager.Data.Models;
+using TaskManager.Data.DTOs;
+using System.Linq;
+
+namespace TaskManager.Service.User
+{
+    public class UserService : IUserService
+    {
+        private IRepository _repo;
+
+        public UserService(IRepository repo)
+        {
+            _repo = repo;   
+        }
+
+        public async System.Threading.Tasks.Task CreateUser(string username, string password)
+        {
+            await _repo.AddAsync(new Data.Models.User() { Username = username, Password = password});
+            await _repo.SaveChangesAsync();
+        }
+
+        public UserDTO[] GetAllUsers()
+        {
+            return _repo.All<Data.Models.User>()
+                .Select(x => new UserDTO
+                {
+                    Username = x.Username,
+                    Password = x.Password
+                })
+                .ToArray();
+        }
+
+        public UserDTO GetUser(string username)
+        {
+            var user = _repo.All<Data.Models.User>().FirstOrDefault(u => u.Username == username);
+
+            if(user == null)
+            {
+                throw new InvalidOperationException("User does not exist");
+            }
+
+            return new UserDTO() { Username = user.Username, Password = user.Password};
+        }
+    }
+}
